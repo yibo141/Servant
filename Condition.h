dc152dc4e2a37262f162fe2974216ddd2de46835
Broglie <yibo141@outlook.com>
2017-02-05 22:04:50 +0800

    add new file: Condition.h

---
+++ Condition.h
@@ -0,0 +1,50 @@
+/*
+ * Author: Broglie 
+ * E-mail: yibo141@outlook.com
+ */
+
+#ifndef CONDITION_H
+#define CONDITION_H
+
+#include <pthread.h>
+#include "Mutex.h"
+
+namespace servant
+{
+
+class Condition 
+{
+public:
+    Condition(Mutex &m): this->mutex(m)
+    {
+        pthread_cond_init(&cond, NULL);
+    }
+
+    ~Condition() 
+    {
+        pthread_cond_destroy(&cond);
+    }
+
+    void wait() 
+    {
+        pthread_cond_wait(&cond, mutex.getPthreadMutex());
+    }
+
+    void notify()
+    {
+        pthread_cond_signal(&cond);
+    }
+
+    void notifyAll()
+    {
+        pthread_cond_broadcast(&cond);
+    }
+
+private:
+    Mutex mutex;
+    pthread_cond_t cond;
+};
+
+}
+
+#endif // CONDITION_H
